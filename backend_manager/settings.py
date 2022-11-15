5dfbe850a4fcc7712034a2ffba9c7eda6c8c55d1
Morris Boers <morrisboers@gmail.com>
2022-11-15 20:09:56 +0100

    created the first models in the database and connected it to have acces to a mysql db

--- backend_manager/settings.py
+++ backend_manager/settings.py
@@ -75,8 +75,12 @@ WSGI_APPLICATION = 'backend_manager.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'sport',
+        'USER': 'root', 
+        'PASSWORD': 'Achter30?', 
+        'HOST': '127.0.0.1', 
+        'PORT': '3306', 
     }
 }
 
@@ -105,7 +109,7 @@ AUTH_PASSWORD_VALIDATORS = [
 
 LANGUAGE_CODE = 'en-us'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'GMT'
 
 USE_I18N = True
 
--- sport/models.py
+++ sport/models.py
@@ -1,3 +1,19 @@
 from django.db import models
 
 # Create your models here.
+
+class Activity(models.Model): 
+    name = models.CharField(max_length=200)
+    description = models.CharField()
+
+class User(models.Model): 
+    name = models.CharField(max_length=200) 
+
+class PersonalActivity(models.Model): 
+    activity = models.ForeignKey(Activity)
+    person = models.ForeignKey(User)
+    date = models.DateTimeField('activity date')
+    weight = models.FloatField(default=0)
+    seat_setting = models.IntegerField(default=1)
+
+
